ee953695fd64c0918beb9ad48ba7688a59c042d0
Ganesh2598 <60608405+Ganesh2598@users.noreply.github.com>
2020-05-21 10:33:37 +0530

    Add files via upload

---
+++ Client/src/Components/Maincontent.js
@@ -0,0 +1,117 @@
+import React , {Component} from 'react'
+
+import List from './Lists'
+import './Main_content_style.css'
+
+
+class Maincontent extends Component{
+
+    constructor(props){
+        super(props)
+        //console.log(props.props.location.state)
+        if(props.props.location.state !== undefined){
+            this.user_id=props.props.location.state.googleusercontent.Ea;
+            
+            window.localStorage.setItem("user_id",this.user_id);
+        }else{
+            this.user_id=window.localStorage.getItem("user_id");
+        }
+        //console.log(this.user_id)
+        this.state={
+            list:[],
+            cur_item:""
+        }
+    }
+
+    inputHandler=(e)=>{
+        this.setState({
+            cur_item:e.target.value
+        })
+       
+    }
+
+    async componentDidMount(){
+        try {
+            const response = await fetch(`/todos/`,{
+                headers:{"Content-Type":"application/json","Accept":"application/json"}
+            });
+            const data = await response.json();
+            console.log(data)
+            const tasks = data.map(obj => obj.task) 
+            this.setState({
+                list:tasks,
+                cur_item:""
+            })
+           
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+
+    addHandler=async e =>{
+        e.preventDefault();
+        try{
+            let task = this.state.cur_item
+            let id = this.user_id
+            const body = {task,id}
+            const response = await fetch("/todos/",{
+                method : "POST",
+                headers : {"Content-Type" : "application/json"},
+                body : JSON.stringify(body)
+            })
+            console.log(response.body)
+        }catch(err){
+            console.log(err)
+        }
+        
+        try {
+            const response = await fetch(`/todos/${this.user_id}`,{
+                headers:{"Content-Type":"application/json","Accept":"application/json"}
+            });
+            const data = await response.json();
+            console.log(data)
+            const tasks = data.map(obj => obj.task) 
+            this.setState({
+                list:tasks,
+                cur_item:""
+            })
+        }catch(err){
+            console.log(err)
+        }
+        
+    }
+
+    deleteHandler= async item=>{
+        try{
+            const itemToDelete=[this.user_id,item.item]
+            const deleted = await fetch(`/todos/${itemToDelete}`,{
+                method : "DELETE"
+            });
+            //console.log(deleted)
+        }catch(err){
+            console.log(err)
+        }
+        //console.log(this.state.list)
+        const updated = this.state.list.filter(task => task !== item.item)
+        this.setState({
+            list:updated
+        })
+        
+    }
+
+
+    render(){
+        return (
+            <div className="main_content">
+                <div className="header">
+                    <input type="text" value={this.state.cur_item} placeholder="Enter Text" onChange={this.inputHandler}/>
+                    <button onClick={this.addHandler}>Add</button>
+                </div>
+                <List props={this.state.list} delete={this.deleteHandler}></List>
+            </div>
+        )
+    }
+}
+
+export default Maincontent
---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("pg").Pool;
+
+
+const path = require("path");
+if (process.env.NODE_ENV === 'production') { 
+    app.use(express.static(path.join(__dirname,"Client/build")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"Client/build/index.html"));
+    })
+ }
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+const dev = "postgresql://postgres:kratos@localhost:5432/todolist";
+const prod = process.env.DATABASE_URL;
+
+const connect = new pool({
+    connectionString:
+    process.env.NODE_ENV=="production" ? prod : dev
+});
+
+
+app.post("/todos/",async (req,res)=>{
+    try{
+        const id = req.body.id;
+        const task = req.body.task;
+        const listOfTask = await connect.query(
+            "INSERT INTO todolist (id,task) VALUES ($1,$2) RETURNING *",[id,task]
+        );
+        res.send(listOfTask.rows[0])
+        //console.log(req.body)
+    }catch(err){
+        console.log(err);
+    }
+})
+
+
+
+app.get("/todos/:user",async (req,res)=>{
+    try{
+        const id = req.params.user;
+        const todoList = await connect.query(
+            "SELECT task FROM todolist WHERE id = $1",[id]
+        );
+        res.json(todoList.rows)
+        console.log(todoList.rows)
+    }catch(err){
+        console.log(err);
+    }
+})
+
+app.delete("/todos/:user",async (req,res)=>{
+    try{
+        const [id,item] = req.params.user.split(",");
+        const updated = await connect.query(
+            "DELETE from todolist where id = $1 and task = $2",[id,item]
+        );
+        res.send("deleted")
+        //console.log("deleted")
+    }catch(err){
+        console.log(err)
+    }
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`server running at ${port}`);
+})
